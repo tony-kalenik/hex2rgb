e18aca16332d3af363c6a46158cf5f07971be914
tony-kalenik <kalenik.tony@gmail.com>
2015-01-11 18:13:23 +0200

    базовый функционал - ввод и перекрашивание фона, с нажатием на кнопку и без проверок введенного

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
-// базовые функции для работы с HEX
+$(function () {
 
-function hexToR(h) {
-    return parseInt((cutHex(h)).substring(0, 2), 16)
-}
-function hexToG(h) {
-    return parseInt((cutHex(h)).substring(2, 4), 16)
-}
-function hexToB(h) {
-    return parseInt((cutHex(h)).substring(4, 6), 16)
-}
-function cutHex(h) {
-    return (h.charAt(0) == "#") ? h.substring(1, 7) : h
-}
+// базовые функции для работы с HEX
+    function hexToR(h) {
+        return parseInt((cutHex(h)).substring(0, 2), 16)
+    }
+
+    function hexToG(h) {
+        return parseInt((cutHex(h)).substring(2, 4), 16)
+    }
+
+    function hexToB(h) {
+        return parseInt((cutHex(h)).substring(4, 6), 16)
+    }
+
+    function cutHex(h) {
+        return (h.charAt(0) == "#") ? h.substring(1, 7) : h
+    }
+
+    var inputHex = $('#hexIn'),
+        outputRGB = $('#rgbOut'),
+        hex = '',
+        rgb = '';
+
+
+    $("button").on({
+        click: function() {
+            hex = inputHex.val();
+            rgb = 'rgb('+hexToR(hex)+','+hexToG(hex)+','+hexToB(hex)+')';
+            $(outputRGB).val(rgb);
+            $("body").css("background-color", '#' + hex);
+        }
+    });
+});
